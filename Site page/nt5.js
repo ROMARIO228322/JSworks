09b3653b22d3583ea7889a66603f89df39d5dc41
Roma <93651044+ROMARIO228322@users.noreply.github.com>
2024-10-17 21:35:53 +0300

    Add files via upload

---
+++ Site page/nt5.js
@@ -0,0 +1,19 @@
+let img = document.getElementById("image");
+function showLinkImage1() {
+  img.src = "kuva1.jpg";
+}
+function showLinkImage2() {
+  img.src = "kuva2.jpg";
+}
+function showLinkImage3() {
+  img.src = "kuva3.jpg";
+}
+function showLinkImage4() {
+  img.src = "kuva4.jpg";
+}
+function showLinkImage5() {
+  img.src = "kuva5.jpg";
+}
+function hideLinkImage() {
+  img.src = "kuva6.jpg";
+}      
